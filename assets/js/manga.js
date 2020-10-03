5b686e9f9f6c3628abebf2f82cea83d05dc02fd9
Yannis <yannis.baltus@outlook.com>
2020-10-03 16:57:22 +0200

    Hide contact form

--- assets/js/manga.js
+++ assets/js/manga.js
@@ -2,15 +2,9 @@ const $contactForm = $('#contactForm');
 
 $(document).ready(function()
 {
-    console.log("debut");
-
     $('#contactBtn').on('click', function(e){
-        console.log("ok");
-        if($contactForm.is(":hidden")){
-            $contactForm.removeAttr('hidden');
-        }
-        else if($contactForm.is(":not(:hidden)")){
-            $contactForm.attr('hidden', $contactForm);
-        }
+        e.preventDefault();
+        $contactForm.slideDown();
+        $('#contactBtn').slideUp();
     });
 })
